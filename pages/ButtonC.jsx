de4865b6d6978279c2fce5a29fbcead1e4cb0396
rupamkanjilal <rupam386595@gmail.com>
2022-10-11 13:07:21 +0530

    final

--- pages/ButtonC.jsx
+++ pages/ButtonC.jsx
@@ -3,7 +3,7 @@ import {BsWhatsapp} from "react-icons/bs"
 function ButtonC(props) {
   return (
     <>
-    <a  href='https://wa.me/917001911814?text=' className={`${props.classItem} mt-[-5px] duration-500 rounded-lg  py-1 lg:py-2 px-6 flex justify-center items-center md:text-xl`}><BsWhatsapp className="mr-2"/> Connect</a>
+    <a  href='https://wa.me/917001911814?text=' className={`${props.classItem} mt-[-5px] duration-500 rounded-lg  py-2 px-6 flex justify-center items-center md:text-xl`}><BsWhatsapp className="mr-2"/> Connect</a>
               
     </>
   )
