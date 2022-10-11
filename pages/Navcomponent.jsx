ccb6586b399b7d3a2f97c11c61b1fcc150e6c0e9
rupamkanjilal <rupam386595@gmail.com>
2022-10-11 13:00:29 +0530

    final

--- pages/Navcomponent.jsx
+++ pages/Navcomponent.jsx
@@ -29,8 +29,8 @@ export default function Navcomponent() {
         // <nav  className={navbar ? "w-full bg-white shadow sticky top-0  transition-all duration-300 py-4": "w-full bg-white shadow py-6"}>
         <nav  className={ navBar=== true ?  "w-full bg-purple-400 shadow-lg z-20 py-4 fixed top-0 ": "w-full bg-[#a460ed] z-20 py-6 " }>
           <div className=" flex justify-between items-center  pr-6 md:px-10 lg:px-20">
-            <div>
-            <Image src={logo1} className="" alt='corelabs'  width='150px' height='50px' />
+            <div className='relative h-[fit] w-[150px] py-2'>
+            <Image src={logo1} className="" alt='corelabs'  objectFit='cover' />
             </div>
             <div>
               <ButtonC classItem={navBar ? "bg-[#5bff6f] hover:bg-[#44be53] text-white shadow-md ":"bg-white hover:bg-gray-200 duration-500 shadow-md shadow-gray-200"}/>
